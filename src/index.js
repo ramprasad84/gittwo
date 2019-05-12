a7c0f644c0211faba0d881ece7bb88645593ca9f
ramprasad84 <ramprasad.ite@gmail.com>
2019-05-12 10:39:01 +0530

    For nodejs

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('This is for nodejs project!');
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
